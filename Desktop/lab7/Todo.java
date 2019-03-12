9094bcc3b2245f40fcbf04a8393e808939fdb1a7
Emily Lu <emily.lu@ou.edu>
2019-03-12 02:49:48 -0500

    todo list added

---
+++ Desktop/lab7/Todo.java
@@ -0,0 +1,49 @@
+public class Todo {/*
+Lab Section_________: 12
+Last Modified_______: 2019-03-12
+OU ID_______________: 113377227
+OU EMAIL____________: emily.lu@ou.edu
+
+Tasks:
+1. Completed methods in HealthyPerson
+2. Completed methods in SickPerson
+3. Completed methods StackHospital
+4. Completed methods QueueHospital
+5. Completed methods PriorityQueueHospital
+6. Writing documentations
+7. Created and finished HealthyPersonTest
+8. Created and finished SickPersonTest
+9. Created and finished StakcHospitalTest
+10. Created and finished QueueHospitalTest
+11. Created and finished PriorityQueueHospitalTest
+12. Debugged
+
+Estimated Time (minutes):
+1. 15
+2. 10
+3. 15
+4. 15
+5. 15
+6. 10
+7. 20
+8. 10
+9. 20
+10. 10
+11.	10
+12. 30
+
+Actual Time (minutes):
+1. 10
+2. 8
+3. 12
+4. 10
+5. 9
+6. 12
+7. 25
+8. 7
+9. 24
+10. 7
+11. 8
+12. 19
+
+*/}
